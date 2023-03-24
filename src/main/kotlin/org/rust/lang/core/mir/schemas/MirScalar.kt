45c75c087a2d2ecb7f7084b5e5d275e8af15b148
Egor Lebedev <lebedev.3gor@gmail.com>
2023-03-25 00:04:43 +0100

    Add toBits function to MirScalar

--- src/main/kotlin/org/rust/lang/core/mir/schemas/MirScalar.kt
+++ src/main/kotlin/org/rust/lang/core/mir/schemas/MirScalar.kt
@@ -7,4 +7,11 @@ package org.rust.lang.core.mir.schemas
 
 sealed class MirScalar {
     data class Int(val scalarInt: MirScalarInt) : MirScalar()
+
+    fun tryToInt(): MirScalarInt? = when (this) {
+        is Int -> scalarInt
+    }
+
+    // TODO: there is error handling done here
+    fun toBits(): Long = tryToInt()?.toBits() ?: error("Could not get bits from scalar")
 }
